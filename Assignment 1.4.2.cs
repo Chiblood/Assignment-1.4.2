e242a8c9c394f725fa80e55e3e22bc5a3d07ddbb
Chiblood <chi.blood6@gmail.com>
2025-08-28 09:49:16 -0400

    Simplified

--- Assignment 1.4.2.cs
+++ Assignment 1.4.2.cs
@@ -1,5 +1,6 @@
 ﻿/* Assignment 1.4.2
 Write a class: “Student” with private data members: StudentId ( int), StudentFname (string), StudentLname (string ), StudentGrade ( char ) and public properties for each data member. Instantiate the class and assign data to properties. Display the data back on console. */
+
 using System;
 class Student
 {
@@ -7,10 +8,10 @@ class Student
     private string StudentFname;
     private string StudentLname;
     private char StudentGrade;
-    public Student(int StudentId, string StudentFname, string StudentLname, char StudentGrade)
+    public Student(int StudentId, string StudentFirstName, string StudentLastName, char StudentGrade)
     {
         this.StudentId = StudentId;
-        this.StudentFname = StudentFname;
+        this.StudentFirstname = StudentFirstName;
         this.StudentLname = StudentLname;
         this.StudentGrade = StudentGrade;
     }
@@ -18,16 +19,27 @@ class Student
     {
         return StudentId;
     }
-    public string GetStudentFname()
+    public string GetStudentFirstName()
     {
-        return StudentFname;
+        return StudentFirstName;
     }
-    public string GetStudentLname()
+    public string GetStudentLastName()
     {
         return StudentLname;
     }
     public char GetStudentGrade()
     {
         return StudentGrade;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student John = new Student(1, "John", "Doe", 'A');
+        Console.WriteLine("Student ID: " + John.GetStudentId());
+        Console.WriteLine("Student First Name: " + John.GetStudentFname());
+        Console.WriteLine("Student Last Name: " + John.GetStudentLname());
+        Console.WriteLine("Student Grade: " + John.GetStudentGrade());
     }
 }
